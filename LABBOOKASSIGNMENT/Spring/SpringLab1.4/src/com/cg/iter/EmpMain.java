ac1d8125d0460196b1949b46f2bf4ca9466ae2cd
gyanaranjandas27 <gyanadas27@gmail.com>
2020-04-16 08:57:47 +0530

    first commit

---
+++ LABBOOKASSIGNMENT/Spring/SpringLab1.4/src/com/cg/iter/EmpMain.java
@@ -0,0 +1,19 @@
+package com.cg.iter;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("emp.xml");
+		   Employee employee=(Employee)ctx.getBean("Preetam");
+		   System.out.println("Employee Details");
+		   System.out.println("--------------------");
+		   System.out.println(employee);
+
+	}
+
+}
