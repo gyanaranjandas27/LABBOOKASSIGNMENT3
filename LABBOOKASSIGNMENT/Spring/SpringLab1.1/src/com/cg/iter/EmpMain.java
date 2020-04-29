b6a2fd3e9b027e622d4f6edd3908111fa2db7e10
gyanaranjandas27 <61134994+gyanaranjandas27@users.noreply.github.com>
2020-04-29 17:06:58 +0530

    Update EmpMain.java

--- LABBOOKASSIGNMENT/Spring/SpringLab1.1/src/com/cg/iter/EmpMain.java
+++ LABBOOKASSIGNMENT/Spring/SpringLab1.1/src/com/cg/iter/EmpMain.java
@@ -7,7 +7,7 @@ public class EmpMain {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("emp.xml");
-		   Employee employee=(Employee)ctx.getBean("Preetam");
+		   Employee employee=(Employee)ctx.getBean("Gyana");
 		   System.out.println("EmployeeDetails");
 		   System.out.println("------------------------");
 		   System.out.println(employee);
